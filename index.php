6e6d59fdd29fcd1acb3082d0503540323e7ee63e
Ijasdeen <ijasdeen23@gmail.com>
2018-06-10 10:44:18 +0530

    Adding
    Adding

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="http://files.risotteriamelottinyc.com/200000406-8600187037/logo risotterira copia - Copia.JPG">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>risotterira copia</title>
+</head>
+<body> 
+   <div class="container">
+       <h3 class="text text-danger">How are you </h3>
+   </div> 
+
+   
+   
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
