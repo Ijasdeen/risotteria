c53036e05a63e258dda3753ecce6becd8eda418c
Ijasdeen <ijasdeen23@gmail.com>
2018-06-10 16:06:01 +0530

    updated

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                                 <div class="col-md-4">
                                     <div class="img-area">
                                         <img src="images/risottoAmaro.jpg" alt="Amaro" class="img-fluid">
-                              <span class="text text-danger">Risotto All'Amarone</span>
+                                        <span class="text text-danger">Risotto All'Amarone</span>
                                     </div>
                                 </div>
                             </div>
@@ -46,13 +46,57 @@
                         <hr/>
                         <div class="delicious-dishes">
                             <h1 class="text text-center">NEW MENU</h1>
+                            <div class="content text-center">
+                                <p class="text text-muted">We have new delicious dishes in our Menu. Come try them to get some summer vibes! </p>
+                                <p>100% gluten-free!</p>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="img-area text text-center">
+                                        <img src="images/bresaola.PNG" alt="Bresaola Carpaccio with brown rice" class="img-fluid">
+                                        <p>Bresaola Carpaccio with brown rice</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="img-area text text-center">
+                                        <img src="images/risottowith.PNG" alt="Risotto with Broccoli rabe & sausages" class="img-fluid">
+                                        <p>Risotto with Broccoli rabe & sausages</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="img-area text text-center">
+                                        <img src="images/risottoCartoccio.PNG" alt="Risotto carattoccoio" class="img-fluid">
+                                        <p>Risotto Al Cartoccio di Crudo with mixed berries</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="img-area text text-center">
+                                        <img src="images/risottowithBeetroot.PNG" alt="risotto with beetroor" class="img-fluid">
+                                        <p>Risotto with Beetroot & Stracchino cheese</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        
-                    </div> <!--<//Inner section>-->
-                    
+
+                    </div>
+                    <!--<//Inner section>-->
+
+                </div>
+                <!--<//Section bg>-->
+            </div>
+            
+            
+            <div class="container">
+                <div class="other-risottors">
+                    <div class="col-md-12">
+                        <div class="header-section text text-center">
+                           <h1 class="text text-danger">OTHERS RISOTTOS FOR YOU!</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
+        
+          </section>
     </div>
 
 </main>
