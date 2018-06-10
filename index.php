adbf1590ff84652bbebf80d2fbd4a8fde31f99c3
Ijasdeen <ijasdeen23@gmail.com>
2018-06-10 15:06:42 +0530

    Updated

--- index.php
+++ index.php
@@ -1,21 +1,59 @@
 <?php require_once('layouts/header.php')?>
-  <main>
-      <div class="container-fluid">
-   <div class="hero-section">
-      <div class="dark-overlay"></div>
-       <div class="restaurant-title">
-          <p>Risotteria Melotti NYC</p>
-            
-       </div>
-   </div>
-   <section class="backround-section">
-       <div class="container">
-           <div class="section-bg"></div>
-       </div>
-   </section>
-  </div>
-      
-  </main>
-   <?php require_once('layouts/footer.php')?>
-    
-   
+<main>
+    <div class="container-fluid">
+        <div class="hero-section">
+            <div class="dark-overlay"></div>
+            <div class="restaurant-title">
+                <p>Risotteria Melotti NYC</p>
+
+            </div>
+        </div>
+        <section class="backround-section">
+            <div class="container">
+                <div class="section-bg">
+                    <div class="inner-section">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h2 class="text text-center">The authentic Italian RISOTTO</h2>
+                            </div>
+                            <div class="col-md-12">
+                                <p class="text text-center">RISOTTO and TRADITIONAL ITALIAN CUISINE - 100% Gluten Free</p>
+                            </div>
+                        </div>
+                        <div class="img-section">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="img-area">
+                                        <img src="images/fung.jpg" alt="Risotto funghi" class="img-fluid">
+                                        <span class="text text-danger">Risotto Funghi Misti</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="img-area">
+                                        <img src="images/risottoLemon.jpg" alt="Risotto lemon" class="img-fluid">
+                                        <span class="text text-danger">Risotto Limone e Gamberi</span>
+
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="img-area">
+                                        <img src="images/risottoAmaro.jpg" alt="Amaro" class="img-fluid">
+                              <span class="text text-danger">Risotto All'Amarone</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div class="delicious-dishes">
+                            <h1 class="text text-center">NEW MENU</h1>
+                        </div>
+                        
+                    </div> <!--<//Inner section>-->
+                    
+                </div>
+            </div>
+        </section>
+    </div>
+
+</main>
+<?php require_once('layouts/footer.php')?>
