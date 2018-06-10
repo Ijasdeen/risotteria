41360a293670773c59b363f55b048622e94f6ebb
Ijasdeen <ijasdeen23@gmail.com>
2018-06-10 10:49:26 +0530

    Files are divided

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="shortcut icon" href="http://files.risotteriamelottinyc.com/200000406-8600187037/logo risotterira copia - Copia.JPG">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>risotterira copia</title>
-</head>
-<body> 
+<?php require_once('layouts/header.php')?>
+  
    <div class="container">
        <h3 class="text text-danger">How are you </h3>
    </div> 
-
    
+   <?php require_once('layouts/footer.php')?>
+    
    
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-</body>
-</html>
---
+++ layouts/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="http://files.risotteriamelottinyc.com/200000406-8600187037/logo risotterira copia - Copia.JPG">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>risotterira copia</title>
+</head>
+<body>
