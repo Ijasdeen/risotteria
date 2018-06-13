328f6cfde8e79c9394d738db50c4faa1025cb7f6
Ijasdeen <ijasdeen23@gmail.com>
2018-06-13 11:10:42 +0530

    updated

--- menus.php
+++ menus.php
@@ -8,6 +8,9 @@
              </div>
           </div>
         </div>
+<!--        <//Hero Section>-->
+       
+       
         <section class="menu-backroundsection">
             <div class="container">
             <div class="section-bg">
@@ -20,11 +23,140 @@
                         <img src="images/ricePasta.PNG" alt="Rice pasta" class="img-fluid">
                      </div>
                      <span>Any addition +$ 4.00</span>
-                     
+                     <hr/>
+                        <div class="menu-area">
+                        <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>ALL'ISOLANA IN "PENTOLINO"</b></p>
+                                 <p>RISOTTO WITH PORK LOIN, FRESH PANCETTA, GRANA PADANO PDO CHEESE, ROSEMARY AND A HINT OF CINNAMO. THE TYPICAL RISOTTO FROM VERONA SERVED IN A SMALL PO</p>
+                             </div>
+                             <div class="p-2 ml-auto">$26.00</div>
+                         </div>
+                         <hr>
+                              <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>VEGETARIANO</b></p>
+                                 <p>RISOTTO WITH MIXED VEGGIES AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$18.00</div>
+                         </div>
+                         <hr>
+                            
+                              <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>SPINACI E ASIAGO D.O.P., RICETTA DI MAMMA ROSETTA</b></p>
+                                 <p>RISOTTO WITH FRESH SPINACH, ASIAGO PDO CHEESE AND GRANA PADANO PDO CHEESE, FROM THE ORIGINAL RECIPE OF MAMMA ROSETTA</p>
+                             </div>
+                             <div class="p-2 ml-auto">$19.00</div>
+                         </div>
+                         <hr> 
+                              <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>FUNGHI MISTI</b></p>
+                                 <p>RISOTTO WITH MIXED WILD ORGANIC MUSHROOMS, PARSLEY AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$21.00</div>
+                         </div>
+                         <hr>
+                         
+                              <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>RAPA ROSSA E STRACCHINO</b></p>
+                                 <p>RISOTTO WITH BEETROOT, STRACCHINO AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$21.00</div>
+                         </div>
+                         <hr>
+                         
+                            <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>POLLO BACON AND ASPARAGI</b></p>
+                                 <p>RISOTTO WITH CHICKEN, BACON, ASPARAGUS AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$21.00</div>
+                         </div>
+                         <hr>
+                            <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>CACIO E PEPE</b></p>
+                                 <p>RISOTTO WITH CACIO CHEESE, FRESH PEPPER AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$21.00</div>
+                         </div>
+                         <hr>
+                            <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>LIMONE E GAMBERI</b></p>
+                                 <p>RISOTTO WITH SHRIMPS, FRESH LEMON JUICE, GRANA PADANO PDO CHEESE AND WHITE WINE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$22.00</div>
+                         </div>
+                         <hr>
+                         <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>ALL'ANATRA</b></p>
+                                 <p>RISOTTO WITH DUCK BREAST RAGU` MADE WITH RED WINE, GRANA PADANO PDO CHEESE, ROSEMARY, SAGE AND BAY LEAVES</p>
+                             </div>
+                             <div class="p-2 ml-auto">$23.00</div>
+                         </div>
+                         <hr>
+                            <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>CAPESANTE, POMODORI SECCHI E BASILICO</b></p>
+                                 <p>RISOTTO WITH SCALLOPS, SUNDRIED TOMATOES, BASIL LEAVES AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$23.00</div>
+                         </div>
+                         <hr>
+                         <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>AL CARTOCCIO DI CRUDO</b></p>
+                                 <p>RISOTTO WITH MIXED BERRIES AND GRANA PADANO PDO CHEESE WRAPPED IN PARMA PROSCIUTTO</p>
+                             </div>
+                             <div class="p-2 ml-auto">$23.00</div>
+                         </div>
+                         <hr>
+                            <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>CIME DI RAPA E SALSICCIA</b></p>
+                                 <p>RISOTTO WITH BROCCOLI RABE, SAUSAGES AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$23.00</div>
+                         </div>
+                         <hr>
+                            <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>AMARONE E MONTE VERONESE</b></p>
+                                 <p>RISOTTO WITH AMARONE WINE SERVED IN A CRISPY GRANA PADANO PDO CHEESE CUP, WITH MONTE VERONESE CHEESE ON TOP</p>
+                             </div>
+                             <div class="p-2 ml-auto">$24.00</div>
+                         </div>
+                         <hr>
+                         
+                            <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>ALLA PESCATORA CON RISO NERO BEPPINO</b></p>
+                                 <p>BLACK RICE WITH FRESH TOMATO SAUCE, SMOKED SALMON, SHRIMP, CALAMARI AND SCALLOPS</p>
+                             </div>
+                             <div class="p-2 ml-auto">$26.00</div>
+                         </div>
+                         <hr>
+                         <div class="d-flex flex-row">
+                             <div class="p-2">
+                                 <p class="text text-danger"><b>AL TARTUFO</b></p>
+                                 <p>RISOTTO WITH FRESH BLACK TRUFFLE AND GRANA PADANO PDO CHEESE</p>
+                             </div>
+                             <div class="p-2 ml-auto">$29.00</div>
+                         </div>
+                         <hr>
+                         
+                      </div> 
+<!--                     <//Menu section>-->
                      
                 </div>
             </div>
             </div>
         </section>
+<!--        <//Backround-section>-->
 </main>
 <?php require_once("layouts/footer.php")?>
