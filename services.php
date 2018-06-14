9309d9f88fbca85034ec7ee5b169d26610f742ef
Ijasdeen <ijasdeen23@gmail.com>
2018-06-14 18:51:48 +0530

    updated

--- services.php
+++ services.php
@@ -24,10 +24,44 @@
      </div>
  </div>
  <div class="container delivery-side">
-     <div class="section-bg">
+     <div class="container">
+         <div class="section-bg">
         <div class="delivery-inner-section">
-            
+          <div class="row">
+              <div class="col-md-7">
+              <img src="images/deliveryPic.PNG" alt="Delivery image" class="img-fluid">
+          </div>
+          <div class="col-md-4 text-center">
+              <div class="content">
+                  <h3>DELIVERY</h3>
+                  <p>Choose between numerous Risottos and other <b>100% gluten free</b> dishes on our menu. Simply place your order on</p>
+                   <h4 class="font-weight-bold">Grubhub or UberEATS.</h4>
+                   <p><i class="font-weight-bold">Risotteria Melotti</i> delivers everything directly where you want it. </p>
+                   <p>This service is available during restaurant hours.</p>
+              </div>
+          </div>
+          </div>
+          <hr/>
+          <div class="content-area-2">
+            <div class="row">
+                <div class="col-md-7 text-center">
+                <div class="content">
+                   <h3 class="text text-danger">CATERING</h3>
+                    <p>Do you have to organize lunches or dinners, business events or private parties?</p>
+                    <p>The Risotteria Melotti will cook the dishes for you, recreating a typically <b>italian  atmosphere</b>in your home or wherever you prefer.</p>
+                    <div class="button-area">
+                        <a href="contact.php" class="btn send-request-button">SEND YOUR REQUEST</a>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-md-4">
+                <img src="images/catering.PNG" alt="Catering picture" class="img-fluid">
+            </div>
+            </div>
+          </div>
         </div>
+     </div>
      </div>
  </div>
  
