e6598772bd7a0a625120005e0f83c80b7899fffa
Ijasdeen <ijasdeen23@gmail.com>
2018-06-23 18:15:29 +0530

    Updated

--- layouts/header.php
+++ layouts/header.php
@@ -10,19 +10,22 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/FontAwesome.otf">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/pureDrawer.css">
     <title>risotterira copia</title>
 </head>
 
 <body>
- 
- 
+     
     <header>
+   
+<!--
        <nav class="navbar navbar-expand-md navbar-light">
+    
             <div class="container">
                 <a href="index.php" class="navbar-brand">
     <img src="images/logi.JPG" alt="Logo" class="img-fluid">
         </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" id="pure-toggle-left" class="pure-toggle" data-toggle="left">
                    <span class="navbar-toggler-icon"></span>
                </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
@@ -45,4 +48,22 @@
                 </div>
             </div>
         </nav>
+-->
+     <div class="pure-container" data-effect="pure-effect-slide">
+    <input type="checkbox" id="pure-toggle-left" class="pure-toggle" data-toggle="left">
+    <label class="pure-toggle-label" for="pure-toggle-left" data-toggle-label="left">
+        <span class="pure-toggle-icon"></span>
+    </label>
+
+    <div class="pure-drawer" data-position="left">
+    
+    </div>
+    <div class="pure-pusher-container">
+        <div class="pure-pusher">
+           
+        </div>
+    </div>
+    <label class="pure-overlay" for="pure-toggle-left" data-overlay="left"></label>
+</div>
+ 
    </header> 
